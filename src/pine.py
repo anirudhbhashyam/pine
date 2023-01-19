4acf7ef409f0a677d629754ddcdb80cdafa51563
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2023-01-19 14:56:24 +0100

    Updated lib with basic shapes.

--- src/pine.py
+++ src/pine.py
@@ -58,6 +58,28 @@ class Circle:
                 if (px - self.x) ** 2 + (py - self.y) ** 2 <= self.r ** 2:
                     im.set_color(py, px, self.color)
 
+class Line:
+    def __init__(self, x1: int, y1: int, x2: int, y2: int, color: int) -> None:
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        self.color = color
+
+    def draw(self, im: Image) -> None:
+        dx = self.x2 - self.x1
+        dy = abs(self.y2 - self.y1)
+        yi = -1 if dy < 0 else 1
+        D = (2 * dy) - dx
+        y = self.y1
+
+        for x in range(self.x1, self.x2):
+            im.set_color(x, y, self.color)
+            if D > 0:
+                y = y + yi
+                D += (2 * (dy - dx))
+            else:
+                D += 2 * dy
 
 
 
