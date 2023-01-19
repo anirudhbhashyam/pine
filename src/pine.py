27159c20b54baa31123a2915495f6c768f3ae249
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2023-01-19 16:48:32 +0100

    Improved line drawing.

--- src/pine.py
+++ src/pine.py
@@ -1,4 +1,9 @@
+import copy 
+
 from typing import Generator 
+
+from dataclasses import dataclass
+
 
 class Image:
     def __init__(self, size: tuple[int, int]) -> None:
@@ -29,25 +34,26 @@ class Image:
                 f.write(f"{r} {g} {b}\n")
 
 
+@dataclass
 class Rectangle:
-    def __init__(self, x: int, y: int, width: int, height: int, color: int) -> None:
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
+    x: int
+    y: int
+    width: int
+    height: int
+    color: int
 
     def draw(self, im: Image) -> None:
         for y in range(self.y, min(self.y + self.height, im.height)):
             for x in range(self.x, min(self.x + self.width, im.width)):
                 im.set_color(x, y, self.color)
 
+
+@dataclass
 class Circle:
-    def __init__(self, x: int, y: int, r: int, color: int) -> None:
-        self.x = x
-        self.y = y
-        self.r = r
-        self.color = color
+    x: int
+    y: int
+    r: int
+    color: int
 
     def draw(self, im: Image) -> None:
         x_min, x_max = self.x - self.r, self.x + self.r
@@ -58,29 +64,59 @@ class Circle:
                 if (px - self.x) ** 2 + (py - self.y) ** 2 <= self.r ** 2:
                     im.set_color(py, px, self.color)
 
+@dataclass
 class Line:
-    def __init__(self, x1: int, y1: int, x2: int, y2: int, color: int) -> None:
-        self.x1 = x1
-        self.x2 = x2
-        self.y1 = y1
-        self.y2 = y2
-        self.color = color
+    x1: int
+    y1: int
+    x2: int
+    y2: int
+    color: int
 
     def draw(self, im: Image) -> None:
-        dx = self.x2 - self.x1
-        dy = abs(self.y2 - self.y1)
-        yi = -1 if dy < 0 else 1
-        D = (2 * dy) - dx
-        y = self.y1
-
-        for x in range(self.x1, self.x2):
+        x = copy.deepcopy(self.x1)
+        y = copy.deepcopy(self.y1)
+        dx = abs(self.x2 - self.x1)
+        sx = 1 if (self.x1 < self.x2) else -1
+        dy = -abs(self.y2 - self.y1)
+        sy = 1 if (self.y1 < self.y2) else -1
+        error = dx + dy
+        
+        while True:
             im.set_color(x, y, self.color)
-            if D > 0:
-                y = y + yi
-                D += (2 * (dy - dx))
-            else:
-                D += 2 * dy
+            if (x == self.x2) and (y == self.y2): 
+                break
+            e2 = error << 1
+            if e2 >= dy:
+                if (x == self.x2): 
+                    break
+                error += dy
+                x += sx
+            if e2 <= dx:
+                if (y == self.y2): 
+                    break
+                error += dx
+                y += sy
 
 
+@dataclass
+class Triangle:
+    x1: int
+    y1: int
+    x2: int
+    y2: int
+    x3: int
+    y3: int
+    color: int
+    _lines: list[Line] = None
 
+    def __post_init__(self) -> None:
+        self._lines = [
+            Line(self.x1, self.y1, self.x2, self.y2, self.color),
+            Line(self.x2, self.y2, self.x3, self.y3, self.color),
+            # Line(self.x3, self.y3, self.x1, self.y1, self.color),
+        ]
+
+    def draw(self, im: Image) -> None:
+        for line in self._lines: 
+            line.draw(im)
 
